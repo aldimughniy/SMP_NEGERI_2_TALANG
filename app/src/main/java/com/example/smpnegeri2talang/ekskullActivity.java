a9b5a82c1d134c5805bccda6d428b3651cc54eaf
aldimughniy <aldioxs4567@gmail.com>
2022-03-08 13:08:59 +0700

    comiit

---
+++ app/src/main/java/com/example/smpnegeri2talang/ekskullActivity.java
@@ -0,0 +1,24 @@
+package com.example.smpnegeri2talang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ekskullActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ekskull2);
+    }
+
+    public void osis(View view) {
+    }
+
+    public void pmr(View view) {
+    }
+
+    public void pramuka(View view) {
+    }
+}
