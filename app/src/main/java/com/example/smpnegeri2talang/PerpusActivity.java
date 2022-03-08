ed69e54139286192bc5b3ebf917fb3a2a3fd087a
aldimughniy <aldioxs4567@gmail.com>
2022-03-08 20:09:00 +0700

    commit

---
+++ app/src/main/java/com/example/smpnegeri2talang/PerpusActivity.java
@@ -0,0 +1,24 @@
+package com.example.smpnegeri2talang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class PerpusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perpus);
+    }
+
+    public void buku7(View view) {
+    }
+
+    public void buku8(View view) {
+    }
+
+    public void buku9(View view) {
+    }
+}
