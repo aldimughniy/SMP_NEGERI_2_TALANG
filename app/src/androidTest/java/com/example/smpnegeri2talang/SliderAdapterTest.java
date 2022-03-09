47c64f9fd27752e98255f757240d53b6be69f5c0
aldimughniy <aldioxs4567@gmail.com>
2022-03-09 19:11:32 +0700

    commit

---
+++ app/src/androidTest/java/com/example/smpnegeri2talang/SliderAdapterTest.java
@@ -0,0 +1,7 @@
+package com.example.smpnegeri2talang;
+
+import junit.framework.TestCase;
+
+public class SliderAdapterTest extends TestCase {
+
+}
---
+++ app/src/main/java/com/example/smpnegeri2talang/SliderAdapterImpl.java
@@ -0,0 +1,7 @@
+package com.example.smpnegeri2talang;
+
+public class SliderAdapterImpl extends SliderAdapter {
+    public SliderAdapterImpl(int[] images) {
+        super(images);
+    }
+}
