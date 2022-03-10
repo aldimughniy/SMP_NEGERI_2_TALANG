5f3c7b39af4ecbdf370c7e3f373fe46d121f1eb0
aldimughniy <aldioxs4567@gmail.com>
2022-03-10 19:24:58 +0700

    commit

--- app/src/main/java/com/example/smpnegeri2talang/ProfilActivity.java
+++ app/src/main/java/com/example/smpnegeri2talang/ProfilActivity.java
@@ -9,6 +9,6 @@ public class ProfilActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profil2);
+        setContentView(R.layout.activity_profil);
     }
 }
