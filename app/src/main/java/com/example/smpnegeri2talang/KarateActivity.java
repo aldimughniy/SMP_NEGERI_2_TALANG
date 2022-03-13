e5a13f9370ce16c11225818e98df0966efdbacea
aldimughniy <aldioxs4567@gmail.com>
2022-03-13 19:57:21 +0700

    commit

--- app/src/main/java/com/example/smpnegeri2talang/KarateActivity.java
+++ app/src/main/java/com/example/smpnegeri2talang/KarateActivity.java
@@ -1,14 +1,14 @@
 package com.example.smpnegeri2talang;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class guruActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class KarateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guru);
+        setContentView(R.layout.activity_karate);
     }
 }
