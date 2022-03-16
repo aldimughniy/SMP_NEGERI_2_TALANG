31130037bd3f4ae66f672beb9fa17de1e6a561a4
aldimughniy <aldioxs4567@gmail.com>
2022-03-16 12:31:14 +0700

    commit

--- app/src/main/java/com/example/smpnegeri2talang/GaleriActivity.java
+++ app/src/main/java/com/example/smpnegeri2talang/GaleriActivity.java
@@ -2,6 +2,7 @@ package com.example.smpnegeri2talang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,18 +14,20 @@ public class GaleriActivity extends AppCompatActivity {
         setContentView(R.layout.activity_galeri);
     }
 
-    public void albumpelantikanosis(View view) {
+    public void albumosis(View view) {
+        Intent intent = new Intent(GaleriActivity.this, albumosisActivity.class);
+        startActivity(intent);
     }
 
-    public void albumperpisahan(View view) {
+
+
+    public void albumhutri(View view) {
+        Intent intent = new Intent(GaleriActivity.this, albumhutriActivity.class);
+        startActivity(intent);
     }
 
-    public void albumkegiatanhutpgri(View view) {
-    }
-
-    public void kegiatan(View view) {
-    }
-
-    public void hutri(View view) {
+    public void kegiatanpramuka(View view) {
+        Intent intent = new Intent(GaleriActivity.this, albumpramukaActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/smpnegeri2talang/SilatActivity.java
@@ -0,0 +1,4 @@
+package com.example.smpnegeri2talang;
+
+public class SilatActivity {
+}
